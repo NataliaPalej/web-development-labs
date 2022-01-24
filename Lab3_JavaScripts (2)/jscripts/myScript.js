1bd4e316a83eb4cc0f9429a5a4d322699eb79793
Natalia Palej <palejnatalia@gmail.com>
2022-01-24 19:04:52 +0000

    Page that prompts user to enter data and store it in table

--- Lab3_JavaScripts (2)/jscripts/myScript.js
+++ Lab3_JavaScripts (2)/jscripts/myScript.js
@@ -1,1 +1,15 @@
-alert("Hello, you are welcome to view my page");
+//alert("Hello, you are welcome to view my page");
+
+function UpdateData()
+{
+    var name = prompt("Enter name: ");
+    var age = prompt("Enter age: ");
+    var height = prompt("Enter height in feet: ")
+    height = height * 30.48;
+    var titleDetails = "Some info on " + name;
+
+    document.getElementById('theTitle').innerHTML = titleDetails;
+    document.getElementById('theName').innerHTML = name;
+    document.getElementById('theAge').innerHTML = age;
+    document.getElementById('theHeight').innerHTML = (height).toFixed(2);
+}
